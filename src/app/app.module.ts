4ccef3cf15b0b355a923bc668617b7e623ff386b
Raquel R <raquelrh@gmail.com>
2018-02-07 21:12:23 +0100

    Modificación de ruta de fichero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BooksModule } from './books/books.module';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
