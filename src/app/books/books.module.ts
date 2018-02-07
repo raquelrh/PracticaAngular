aba6c67a150df2892844172f02f8376bbab887ce
Raquel R <raquelrh@gmail.com>
2018-02-07 22:23:19 +0100

    Se añaden los exports

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -3,16 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
 import { BookComponent } from './book/book.component';
-import { BookService } from './shared/book.service';
 import { ListBookComponent } from './list-book/list-book.component';
 import { DetailBookComponent } from './detail-book/detail-book.component';
 import { PageBookComponent } from './page-book/page-book.component';
+import { BookService } from './shared/book.service';
 
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule
   ],
+  exports: [BookComponent, ListBookComponent, DetailBookComponent, PageBookComponent], //por defecto no xporta los componentes
   declarations: [BookComponent, ListBookComponent, DetailBookComponent, PageBookComponent],
   providers: [BookService]
 })
